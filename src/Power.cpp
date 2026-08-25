b0c60d0281c20743bd74dfe19b0d04f3c9bad806
bibabaibur-eng <bibabaibur@gmail.com>
2026-08-25 21:30:36 +0300

    Update Power.cpp

--- src/Power.cpp
+++ src/Power.cpp
@@ -14,10 +14,8 @@
  * For more information, see: https://meshtastic.org/
  */
 #include "power.h"
-#ifdef CONFIG_IDF_TARGET_ESP32C3
 #ifndef ADC_CHANNEL
 #define ADC_CHANNEL ADC1_CHANNEL_1
-#endif
 #endif
 
 #include "MessageStore.h"
